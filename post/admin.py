ab6a6142be9a2947b2c2684fcffc9172402ec736
Viva1-ua <veronikaboeva935@gmail.com>
2025-03-18 21:09:26 +0100

    Adding verbose_name to models and register sites in admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from post.models import Commentary, Post, User, Followers, Topic
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name", )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("topic", "publisher", "created_at", )
+    search_fields = ("topic", "publisher", )
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ("author", "post", "created_at", )
+    search_fields = ("author", "post", )
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("username", "email", "date_joined", )
+    search_fields = ("username", )
+    list_filter = ("email", "date_joined", )
+
+
+@admin.register(Followers)
+class FollowersAdmin(admin.ModelAdmin):
+    list_display = ("user", "followers", )
+
+
--- post/models.py
+++ post/models.py
@@ -1,3 +1,67 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name", ]
+        verbose_name = "Topics"
+
+
+class Post(models.Model):
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE)
+    content = models.TextField()
+    publisher = models.ForeignKey("User", on_delete=models.CASCADE)
+    comments = models.ManyToManyField("Commentary", blank=True, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.topic
+
+    class Meta:
+        ordering = ["-created_at", "topic", ]
+        verbose_name = "Posts"
+
+
+class Commentary(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="post")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "Commentarys"
+
+
+class User(AbstractUser):
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ["username", ]
+        verbose_name = "Users"
+
+
+class Followers(models.Model):
+    followers = models.ForeignKey(
+        "User", on_delete=models.CASCADE,
+        related_name="followers"
+    )
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE,
+        related_name="following_user",
+        verbose_name="Following"
+    )
+
+    class Meta:
+        verbose_name = "Followers"
