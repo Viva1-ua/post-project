2c75c0751ee891262772d1c9ae8f4e95f7f48b7b
Viva1-ua <veronikaboeva935@gmail.com>
2025-04-03 13:06:50 +0200

    View.py modify

--- post/views.py
+++ post/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from post.models import Post, User, Field, Commentary
+from post.models import Post, User, Field
 
 
 @login_required
